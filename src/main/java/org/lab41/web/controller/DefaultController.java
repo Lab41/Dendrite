a5439e30e405ab63280a74c2a5bed61248de9d31
Erick Tryzelaar <erickt@lab41.org>
2013-06-17 16:48:05 -0700

    Starting to frame out a web backend

---
+++ src/main/java/org/lab41/web/controller/DefaultController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lab41.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author etryzelaar
+ */
+@Controller
+public class DefaultController {
+
+    @ResponseBody
+
+}
