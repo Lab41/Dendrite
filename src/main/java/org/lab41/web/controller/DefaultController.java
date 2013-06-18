5c965348f851780faa89c1edb6eb96fca4cd1a35
Erick Tryzelaar <erickt@lab41.org>
2013-06-18 15:17:40 -0700

    Start framing out a Titan graph service

--- src/main/java/org/lab41/web/controller/DefaultController.java
+++ src/main/java/org/lab41/web/controller/DefaultController.java
@@ -6,6 +6,8 @@ package org.lab41.web.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.lab41.titan.GraphService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class DefaultController {
+
+    @Autowired
+    private GraphService graphService;
 
     @RequestMapping("/items")
     public @ResponseBody List<String> getItems() {
