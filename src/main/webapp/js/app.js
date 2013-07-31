40289ca28158ff78ec7f80b7a059bc338ca40807
Erick Tryzelaar <erickt@lab41.org>
2013-07-31 15:39:35 -0700

    Integrate AngularJS authentication
        Update Angular webapp
        - Add Angular services and controllers for authentication
    
        Incomplete:
        - Initial Angular unit tests (need to resolve HTTPsession to Spring backend)

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -10,10 +10,19 @@ angular.module('dendrite', [
     ]).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.
+        when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'}).
         when('/graphs', {templateUrl: 'partials/graph-list.html', controller: 'GraphListCtrl'}).
         when('/graphs/:graphId', {templateUrl: 'partials/graph-detail.html', controller: 'GraphDetailCtrl'}).
         when('/graphs/:graphId/vertices', {templateUrl: 'partials/vertex-list.html', controller: 'VertexListCtrl'}).
         when('/graphs/:graphId/vertices/:vertexId', {templateUrl: 'partials/vertex-detail.html', controller: 'VertexDetailCtrl'}).
         when('/graphs/:graphId/create_vertex', {templateUrl: 'partials/vertex-create.html', controller: 'VertexCreateCtrl'}).
         otherwise({redirectTo: '/graphs'});
-  }]);
+  }]).
+  // redirect all unauthenticated users to login page, regardless of intended route
+  run(function($rootScope, $location, User) {
+    return $rootScope.$on('$routeChangeStart', function(event, next, current) {
+      if (!User.isAuthenticated() && next.templateUrl !== '/partials/login.html') {
+        return $location.path("/login");
+      }
+    });
+  });
