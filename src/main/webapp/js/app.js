bade98113c40e61853878a0a7b9d0d5c3f259dd8
Kyle F <kyle@iqt.org>
2013-08-01 11:22:30 -0700

    Update webapp with Angular authentication interceptor
    Authentication interceptor enables
    - redirection to login URL for unauthenticated visitors
    - intercept 401 responses from SpringMVC for custom actions
    
    Incomplete: End-to-End testing in Angular
    - Ruby watir test as authentication verifier until E2E testing complete

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -18,11 +18,106 @@ angular.module('dendrite', [
         when('/graphs/:graphId/create_vertex', {templateUrl: 'partials/vertex-create.html', controller: 'VertexCreateCtrl'}).
         otherwise({redirectTo: '/graphs'});
   }]).
-  // redirect all unauthenticated users to login page, regardless of intended route
-  run(function($rootScope, $location, User) {
-    return $rootScope.$on('$routeChangeStart', function(event, next, current) {
-      if (!User.isAuthenticated() && next.templateUrl !== '/partials/login.html') {
-        return $location.path("/login");
+  config(['$httpProvider', function($httpProvider) {
+    var interceptor = ['$rootScope','$q', function(scope, $q) {
+   
+      function success(response) {
+        return response;
+      }
+   
+      function error(response) {
+      
+        // notify user if login incorrect
+        if (response.config.url === scope.url_login){
+          alert("Username or Password Incorrect!");
+        }
+
+        var status = response.status;   
+        if (status === 401) {
+          var deferred = $q.defer();
+          var req = {
+            config: response.config,
+            deferred: deferred
+          }
+          scope.requests401.push(req);
+          scope.$broadcast('event:loginRequired');
+          return deferred.promise;
+        }
+        // otherwise
+        return $q.reject(response);
+   
+      }
+   
+      return function(promise) {
+        return promise.then(success, error);
+      }
+   
+    }];
+    
+    // add interceptor to app
+    $httpProvider.responseInterceptors.push(interceptor);
+  }]).
+  run(['$rootScope', '$http', '$location', 'User', function(scope, $http, $location, User) {
+
+    // store requests which failed due to 401 response.
+    scope.requests401 = [];
+    
+    // store auth URLs
+    scope.url_login = 'j_spring_security_check';
+    scope.url_logout = 'j_spring_security_logout';
+   
+    // event:loginConfirmed - resend all the 401 requests.
+    scope.$on('event:loginConfirmed', function() {
+      var i, requests = scope.requests401;
+      for (i = 0; i < requests.length; i++) {
+        retry(requests[i]);
+      }
+      scope.requests401 = [];
+   
+      function retry(req) {
+        $http(req.config).then(function(response) {
+          req.deferred.resolve(response);
+        });
       }
     });
-  });
+    
+    //event:loginRequest - send credentials to the server.
+    scope.$on('event:loginRequest', function(event, username, password) {
+      var payload = $.param({j_username: username, j_password: password});
+      var config = {
+        headers: {'Content-Type':'application/json; charset=UTF-8'}
+      }
+      $http.post(scope.url_login, payload, config).success(function(data) {
+        if (data === 'AUTHENTICATION_SUCCESS') {
+          scope.$broadcast('event:loginConfirmed');
+        }
+      });
+    });
+
+    // event:loginRequired - redirect to root URL
+    scope.$on('event:loginRequired', function() {
+      $location.path("/login");
+    });
+    
+    // event: logoutConfirmed - redirect to root URL
+    scope.$on('event:logoutConfirmed', function() {
+      $location.path("/login");
+      alert("Thanks for Dendrite-ing!  Come back soon, y'all!");
+    });
+       
+    // event: logoutRequest - invoke logout on the server and broadcast 'event:loginRequired'.
+    scope.$on('event:logoutRequest', function() {
+      $http.put(scope.url_logout, {}).success(function() {
+        ping();
+      });
+    });
+   
+    // placeholder function to test login status
+    function ping() {
+      $http.get('rest/ping').success(function() {
+        scope.$broadcast('event:loginConfirmed');
+      });
+    }
+    //ping();
+    
+  }]);
