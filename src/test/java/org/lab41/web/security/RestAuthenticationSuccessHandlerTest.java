fb0a794a8645a6730d35827ef06326d37f316dc5
Erick Tryzelaar <erickt@lab41.org>
2013-08-28 15:21:28 -0700

    Fix the java authentication tests

--- src/test/java/org/lab41/web/security/RestAuthenticationSuccessHandlerTest.java
+++ src/test/java/org/lab41/web/security/RestAuthenticationSuccessHandlerTest.java
@@ -23,21 +23,18 @@ import static org.junit.Assert.assertEquals;
 /**
  * @author kramachandran
  */
-public class JsonAuthenticationSuccessHandlerTest {
-    private JsonAuthenticationSuccessHandler jsonRestAuthenticationSuccessHandler;
+public class RestAuthenticationSuccessHandlerTest {
+    private RestAuthenticationSuccessHandler restAuthenticationSuccessHandler;
     private ObjectMapper mapper = new ObjectMapper();
 
 
     @After
     public void tearDown() throws Exception {
-
-
     }
 
     @Before
     public void setUp() throws Exception {
-        jsonRestAuthenticationSuccessHandler = new JsonAuthenticationSuccessHandler();
-
+        restAuthenticationSuccessHandler = new RestAuthenticationSuccessHandler();
     }
 
     @Test
@@ -49,28 +46,10 @@ public class JsonAuthenticationSuccessHandlerTest {
         MockHttpServletRequest request = new MockHttpServletRequest();
         MockHttpServletResponse response = new MockHttpServletResponse();
 
-        jsonRestAuthenticationSuccessHandler.onAuthenticationSuccess(request, response, authentication);
+        restAuthenticationSuccessHandler.onAuthenticationSuccess(request, response, authentication);
 
-        assertEquals(response.getStatus(), HttpServletResponse.SC_OK);
-
-        //Expected Response
-        /*
-            {"details":
-                {"remoteAddress":"0:0:0:0:0:0:0:1",
-                 "sessionId":"AEA0E8D38618341134B52FCE68CE5CE7"},
-                 "authorities":[{"authority":"ROLE_USER"}],
-                 "authenticated":true,
-                 "principal":{"password":null,"username":"user","authorities":[{"authority":"ROLE_USER"}],
-                 "accountNonExpired":true,
-                 "accountNonLocked":true,
-                 "credentialsNonExpired":true,"enabled":true},
-                 "credentials":null,
-                 "name":"user"}
-         */
-        Map object = (Map) mapper.readValue(response.getContentAsString(), Object.class);
-        assertEquals(object.get("name"), "user");
-
-
+        assertEquals(response.getStatus(), HttpServletResponse.SC_NO_CONTENT);
+        assertEquals(response.getContentAsString(), "");
     }
 
 }
