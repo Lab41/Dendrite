612ad66e26bd596c86fc04aca39d8b262ec54566
Erick Tryzelaar <erickt@lab41.org>
2013-10-03 11:17:33 -0700

    Add ASL2 license

--- src/main/java/org/lab41/jersey/providers/RexsterApplicationProvider.java
+++ src/main/java/org/lab41/jersey/providers/RexsterApplicationProvider.java
@@ -1,12 +1,20 @@
-package org.lab41.jersey.providers;
-
 /**
- * Created with IntelliJ IDEA.
- * User: kramachandran-admin
- * Date: 8/4/13
- * Time: 7:49 PM
- * To change this template use File | Settings | File Templates.
+ * Copyright 2013 In-Q-Tel/Lab41
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+package org.lab41.jersey.providers;
 
 import com.sun.jersey.api.core.HttpContext;
 import com.sun.jersey.core.spi.component.ComponentContext;
