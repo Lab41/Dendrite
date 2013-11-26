3294368caafd10d6c4c0075b14be8a75ca3f6621
Erick Tryzelaar <erickt@lab41.org>
2013-11-26 09:28:28 -0800

    Move the java packages under org.lab41.dendrite

---
+++ src/test/java/org/lab41/dendrite/faunus/AdjacencyListOutputFormatTest.java
@@ -0,0 +1,13 @@
+package org.lab41.dendrite.faunus;
+
+import static org.junit.Assert.assertTrue;
+
+public class AdjacencyListOutputFormatTest {
+
+    /*
+    @Test
+    public void testInGremlinImports() {
+        assertTrue(Imports.getImports().contains(AdjacencyFileOutputFormat.class.getPackage().getName() + ".*"));
+    }
+    */
+}
