358c97ac19723699e9c33bc6449cf3972d21ad2e
Kyle F <kylef@iqt.org>
2013-12-10 11:26:47 -0800

    Add backend class to save graph server-side
    API endpoint "file-save" saves the graph to the
    HistoryService-defined storage location, which is
    envisioned to be a .git repository for committing
    changes/actions captured through the UI.

--- src/main/java/org/lab41/web/controller/GraphExportController.java
+++ src/main/java/org/lab41/web/controller/GraphExportController.java
@@ -35,6 +35,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.lab41.services.HistoryService;
+
 import java.io.IOError;
 import java.io.IOException;
 
@@ -45,6 +47,9 @@ public class GraphExportController {
 
     @Autowired
     DendriteRexsterApplication application;
+
+    @Autowired
+    HistoryService historyService;
 
     @RequestMapping(value = "/api/{graphName}/file-export", method = RequestMethod.POST)
     public void export(@PathVariable String graphName, GraphExportBean exportItem, HttpServletResponse response, BindingResult result) {
@@ -89,4 +94,48 @@ public class GraphExportController {
         }
     }
 
+    @RequestMapping(value = "/api/{graphName}/file-save", method = RequestMethod.POST)
+    public void save(@PathVariable String graphName, GraphExportBean exportItem, HttpServletResponse response, BindingResult result) {
+
+        JSONObject json = new JSONObject();
+
+        if (result.hasErrors()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        if (exportItem.getFormat() == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        Graph graph = application.getGraph(graphName);
+        if (graph == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // extract the storage location for the history
+        String historyStorageLocation = historyService.getHistoryStorage();
+
+        String format = exportItem.getFormat();
+        try {
+
+            if (format.equalsIgnoreCase("GraphSON")) {
+                GraphSONWriter.outputGraph(graph, historyStorageLocation+"/"+graphName+".json");
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else if (format.equalsIgnoreCase("GraphML")) {
+                GraphMLWriter.outputGraph(graph, historyStorageLocation+"/"+graphName+".xml");
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else if (format.equalsIgnoreCase("GML")) {
+                GMLWriter.outputGraph(graph, historyStorageLocation+"/"+graphName+".gml");
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+        } catch (IOException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+    }
 }
