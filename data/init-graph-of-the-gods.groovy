6da0813c4b785a4651d1ad655ce1e9d4837e4d70
Erick Tryzelaar <erickt@lab41.org>
2013-12-13 11:01:26 -0800

    Add graph creation to init-graph-of-the-gods.grooy
    Now that Elasticsearch indices are created automatically we
    can enable support for Geoshape indices. Unfortunately graphson
    doesn't have any way to express a Geoshape so we now do the
    graph creation in the groovy script.

--- data/init-graph-of-the-gods.groovy
+++ data/init-graph-of-the-gods.groovy
@@ -1,26 +1,86 @@
+import com.thinkaurelius.titan.core.TitanKey
+import com.thinkaurelius.titan.core.attribute.Geoshape
+import com.tinkerpop.blueprints.Edge
+import com.tinkerpop.blueprints.Vertex
+import com.tinkerpop.blueprints.util.ElementHelper
+
 g.makeKey("name").dataType(String.class).indexed(Vertex.class).unique().make()
-g.makeKey("age").dataType(Integer.class).indexed(Vertex.class).make()
+g.makeKey("age").dataType(Integer.class).indexed("search", Vertex.class).make()
 g.makeKey("type").dataType(String.class).make()
 
-time = g.makeKey("time").dataType(Integer.class).make()
-reason = g.makeKey("reason").dataType(String.class).indexed(Edge.class).make()
-//g.makeKey("place").dataType(Geoshape.class).indexed(Edge.class).make()
-g.makeKey("place").dataType(String.class).indexed(Edge.class).make()
+TitanKey time = g.makeKey("time").dataType(Integer.class).make()
+TitanKey reason = g.makeKey("reason").dataType(String.class).indexed("search", Edge.class).make()
+g.makeKey("place").dataType(Geoshape.class).indexed("search", Edge.class).make()
 
-/*
 g.makeLabel("father").manyToOne().make()
 g.makeLabel("mother").manyToOne().make()
 g.makeLabel("battled").sortKey(time).make()
 g.makeLabel("lives").signature(reason).make()
 g.makeLabel("pet").make()
 g.makeLabel("brother").make()
-*/
-
-g.makeLabel("father").make()
-g.makeLabel("mother").make()
-g.makeLabel("battled").sortKey(time).make()
-g.makeLabel("lives").signature(reason).make()
-g.makeLabel("pet").make()
-g.makeLabel("brother").make()
 
 g.commit()
+
+Vertex saturn = g.addVertex(null);
+saturn.setProperty("name", "saturn");
+saturn.setProperty("age", 10000);
+saturn.setProperty("type", "titan");
+
+Vertex sky = g.addVertex(null);
+ElementHelper.setProperties(sky, "name", "sky", "type", "location");
+
+Vertex sea = g.addVertex(null);
+ElementHelper.setProperties(sea, "name", "sea", "type", "location");
+
+Vertex jupiter = g.addVertex(null);
+ElementHelper.setProperties(jupiter, "name", "jupiter", "age", 5000, "type", "god");
+
+Vertex neptune = g.addVertex(null);
+ElementHelper.setProperties(neptune, "name", "neptune", "age", 4500, "type", "god");
+
+Vertex hercules = g.addVertex(null);
+ElementHelper.setProperties(hercules, "name", "hercules", "age", 30, "type", "demigod");
+
+Vertex alcmene = g.addVertex(null);
+ElementHelper.setProperties(alcmene, "name", "alcmene", "age", 45, "type", "human");
+
+Vertex pluto = g.addVertex(null);
+ElementHelper.setProperties(pluto, "name", "pluto", "age", 4000, "type", "god");
+
+Vertex nemean = g.addVertex(null);
+ElementHelper.setProperties(nemean, "name", "nemean", "type", "monster");
+
+Vertex hydra = g.addVertex(null);
+ElementHelper.setProperties(hydra, "name", "hydra", "type", "monster");
+
+Vertex cerberus = g.addVertex(null);
+ElementHelper.setProperties(cerberus, "name", "cerberus", "type", "monster");
+
+Vertex tartarus = g.addVertex(null);
+ElementHelper.setProperties(tartarus, "name", "tartarus", "type", "location");
+
+// edges
+
+jupiter.addEdge("father", saturn);
+jupiter.addEdge("lives", sky).setProperty("reason", "loves fresh breezes");
+jupiter.addEdge("brother", neptune);
+jupiter.addEdge("brother", pluto);
+
+neptune.addEdge("lives", sea).setProperty("reason", "loves waves");
+neptune.addEdge("brother", jupiter);
+neptune.addEdge("brother", pluto);
+
+hercules.addEdge("father", jupiter);
+hercules.addEdge("mother", alcmene);
+ElementHelper.setProperties(hercules.addEdge("battled", nemean), "time", 1, "place", Geoshape.point(38.1f, 23.7f));
+ElementHelper.setProperties(hercules.addEdge("battled", hydra), "time", 2, "place", Geoshape.point(37.7f, 23.9f));
+ElementHelper.setProperties(hercules.addEdge("battled", cerberus), "time", 12, "place", Geoshape.point(39f, 22f));
+
+pluto.addEdge("brother", jupiter);
+pluto.addEdge("brother", neptune);
+pluto.addEdge("lives", tartarus).setProperty("reason", "no fear of death");
+pluto.addEdge("pet", cerberus);
+
+cerberus.addEdge("lives", tartarus);
+
+g.commit();
