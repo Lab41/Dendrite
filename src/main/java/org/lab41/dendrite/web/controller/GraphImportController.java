246134fd66a1698a7dd802a9224d33b49fa3ac2b
Kyle F <kylef@iqt.org>
2014-01-15 12:04:43 -0800

    Add importer for Faunus GraphSON format

--- src/main/java/org/lab41/dendrite/web/controller/GraphImportController.java
+++ src/main/java/org/lab41/dendrite/web/controller/GraphImportController.java
@@ -39,9 +39,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
 import javax.validation.Valid;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ByteArrayInputStream;
+import java.io.BufferedReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Arrays;
@@ -53,6 +59,58 @@ public class GraphImportController {
 
     @Autowired
     MetaGraphService metaGraphService;
+
+    // faunus->standard graphSON converter
+    // modified from https://github.com/WhySearchTwice/Data-and-Rest-Services
+    private static JSONObject loadAndTransformFaunusFormat(InputStream inputStream) throws Exception {
+        JSONArray edges = new JSONArray();
+        JSONArray vertices = new JSONArray();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = br.readLine()) != null) {
+            JSONObject json = new JSONObject(line);
+
+            if (json.has("_inE")) {
+                // create edges object
+                JSONArray inE = json.getJSONArray("_inE");
+                for (int i = 0; i < inE.length(); i++) {
+                  JSONObject edge = inE.getJSONObject(i);
+                  edge.put("_inV", json.get("_id"));
+                  edges.put(edge);
+                }
+
+                // remove edges from intended vertex list
+                json.remove("_inE");
+            }
+
+            if (json.has("_outE")) {
+
+              // create edges object
+              JSONArray outE = json.getJSONArray("_outE");
+              for (int i = 0; i < outE.length(); i++) {
+                JSONObject edge = outE.getJSONObject(i);
+                edge.put("_outV", json.get("_id"));
+                edges.put(edge);
+              }
+
+              // remove edges from intended vertex list
+              json.remove("_outE");
+            }
+
+            // create vertex object
+            vertices.put(json);
+        }
+        br.close();
+
+        JSONObject graphJSON = new JSONObject();
+        graphJSON.put("mode", "NORMAL");
+        graphJSON.put("vertices", vertices);
+        graphJSON.put("edges", edges);
+        JSONObject graphWrapper = new JSONObject();
+        graphWrapper.put("graph", graphJSON);
+        return graphWrapper;
+    }
 
     @RequestMapping(value = "/api/graphs/{graphId}/file-import", method = RequestMethod.POST)
     public ResponseEntity<Map<String, Object>> importGraph(@PathVariable String graphId,
@@ -109,6 +167,11 @@ public class GraphImportController {
                 GraphMLReader.inputGraph(graph, inputStream);
             } else if (format.equalsIgnoreCase("GML")) {
                 GMLReader.inputGraph(graph, inputStream);
+            } else if (format.equalsIgnoreCase("FaunusGraphSON")) {
+                JSONObject json = loadAndTransformFaunusFormat(inputStream);
+                InputStream in = new ByteArrayInputStream(json.toString().getBytes());
+                GraphSONReader.inputGraph(graph, in);
+                in.close();
             } else {
                 response.put("status", "error");
                 response.put("msg", "unknown format '" + format + "'");
@@ -116,7 +179,7 @@ public class GraphImportController {
                 return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
             }
             inputStream.close();
-        } catch(IOException e) {
+        } catch(Exception e) {
             response.put("status", "error");
             response.put("msg", "exception: " + e.toString());
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
