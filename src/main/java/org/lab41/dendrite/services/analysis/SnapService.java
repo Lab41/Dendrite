422265c9dda33ccb32f0747e895a32086b039a0a
Charlie Lewis <charliel@lab41.org>
2014-07-11 13:34:23 -0700

    add missing imports

--- src/main/java/org/lab41/dendrite/services/analysis/SnapService.java
+++ src/main/java/org/lab41/dendrite/services/analysis/SnapService.java
@@ -14,6 +14,8 @@ import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsAction;
+import org.apache.hadoop.fs.permission.FsPermission;
 import org.lab41.dendrite.jobs.FaunusJob;
 import org.lab41.dendrite.metagraph.DendriteGraph;
 import org.lab41.dendrite.metagraph.models.JobMetadata;
