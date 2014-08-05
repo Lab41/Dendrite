7463f8bdd8dc4e2a6af4be652a05edae9beb1837
Karthik Ramachandran <kramachandran@iqt.org>
2014-08-04 23:25:52 -0400

    WIP -flushed out GraphPermissionHandler

--- src/main/java/org/lab41/dendrite/web/security/GraphPermissonHandler.java
+++ src/main/java/org/lab41/dendrite/web/security/GraphPermissonHandler.java
@@ -1,7 +1,14 @@
 package org.lab41.dendrite.web.security;
 
+import org.lab41.dendrite.metagraph.MetaGraph;
+import org.lab41.dendrite.metagraph.MetaGraphTx;
+import org.lab41.dendrite.metagraph.models.BranchMetadata;
+import org.lab41.dendrite.metagraph.models.ProjectMetadata;
+import org.lab41.dendrite.metagraph.models.UserMetadata;
+import org.lab41.dendrite.services.MetaGraphService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 
@@ -12,6 +19,63 @@ import java.io.Serializable;
  */
 public class GraphPermissonHandler implements PermissionEvaluator{
     Logger logger = LoggerFactory.getLogger("GraphPermissionHandler");
+
+    @Autowired
+    MetaGraphService metaGraphService;
+
+    //TODO: Figure out this should be done as a try/finally
+
+    boolean checkUserForBranch(Authentication authentication, Serializable targetId)
+    {
+        MetaGraphTx tx = metaGraphService.buildTransaction().readOnly().start();
+        BranchMetadata branchMetadata = tx.getBranch((String)targetId);
+        ProjectMetadata projectMetadata = branchMetadata.getProject();
+        if (checkAuthAgainsUserOfProject(authentication, projectMetadata))
+            return true;
+        tx.commit();
+       return false ;
+    }
+
+    private boolean checkAuthAgainsUserOfProject(Authentication authentication, ProjectMetadata projectMetadata) {
+        for(UserMetadata user : projectMetadata.getUsers())
+        {
+            if(user.getName().equals(authentication.getName()))
+                return true;
+        }
+        return false;
+    }
+
+    boolean checkUserForProject(Authentication authentication, Serializable targetId)
+    {
+        MetaGraphTx tx = metaGraphService.buildTransaction().readOnly().start();
+        ProjectMetadata projectMetadata = tx.getProject((String)targetId);
+        if (checkAuthAgainsUserOfProject(authentication, projectMetadata))
+            return true;
+        tx.commit();
+        return false;
+    }
+
+    boolean checkUserForGraph(Authentication authentication, Serializable targetId)
+    {
+        MetaGraphTx tx = metaGraphService.buildTransaction().readOnly().start();
+        ProjectMetadata projectMetadata = tx.getGraph((String)targetId).getProject();
+        if (checkAuthAgainsUserOfProject(authentication, projectMetadata))
+            return true;
+        tx.commit();
+        return false;
+    }
+
+    boolean checkUserForJob(Authentication authentication, Serializable targetId)
+    {
+        MetaGraphTx tx = metaGraphService.buildTransaction().readOnly().start();
+        ProjectMetadata projectMetadata = tx.getJob((String)targetId).getProject();
+        if (checkAuthAgainsUserOfProject(authentication, projectMetadata))
+            return true;
+        tx.commit();
+        return false;
+
+    }
+
     @Override
     public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
         //TODO fill this out
@@ -20,7 +84,23 @@ public class GraphPermissonHandler implements PermissionEvaluator{
 
     @Override
     public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
-        logger.debug("In has permission"+targetId+targetType+permission);
-        return false;
+        boolean  retVal= false;
+        switch(targetType) {
+            case "project":
+                retVal = checkUserForProject(authentication, targetId);
+                break;
+            case "graph":
+                retVal = checkUserForGraph(authentication, targetId);
+                break;
+            case "branch":
+                retVal = checkUserForBranch(authentication, targetId);
+                break;
+            case "job":
+                retVal = checkUserForJob(authentication, targetId);
+                break;
+            default:
+                retVal = false;
+        }
+        return retVal;
     }
 }
