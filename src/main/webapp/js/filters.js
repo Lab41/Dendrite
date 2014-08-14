b6a98fb84a8b81b92612bed5b61d602aed6649f8
Kyle F <kylef@iqt.org>
2014-08-14 10:29:50 -0700

    UI: Add filter for JSON pretty-print

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -42,4 +42,27 @@ angular.module('dendrite.filters', [])
       }
       return retVal;
     }
+  })
+  // prettify from http://jsfiddle.net/KSTe8/
+  .filter('prettify', function () {
+    return function(json, scope) {
+        json = JSON.stringify(json, undefined, 4);
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            match = match.replace(/"/g, '');
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
   });
